e8ae5277e6baa384820ab44bb058343f497cfe56
jose diaz <jose7312000@gmail.com>
2021-06-21 12:29:15 -0500

    Trabajando con el controlador

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Agreganos las rutas de la api
+
+//Muestra solo la ruta show
+Route::apiResource('v1/posts', 'PostController')->only('show');
+
+//Mustra toda las rutas
+Route::apiResource('v1/posts', 'PostController');
